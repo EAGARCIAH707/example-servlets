ccab7d1907ed8d639c6f972e540382b8518dfcc6
EAGARCIAH707 <eagarciah707@gmail.com>
2020-09-01 20:28:28 -0500

    Initial commit

---
+++ src/main/java/com/andevs/servlets/ServletsApplication.java
@@ -0,0 +1,13 @@
+package com.andevs.servlets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServletsApplication.class, args);
+    }
+
+}
